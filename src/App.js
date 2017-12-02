8e7cd65efba9657c2ac49f96857ac39e74a64fc2
Kallirroi <kallirroi.retzepi@gmail.com>
2017-12-01 20:50:56 -0500

    adding content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import What from './What';
+import Stories from './Stories';
+import Cards from './Cards';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header> the benches </header>
+        <What/>
+        <Stories/>
+        <Cards/>
       </div>
     );
   }
---
+++ src/Cards.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class What extends Component {
+  render() {
+    return (
+      <div className="block">
+        <div className="subtitle">
+        where are the stories from?
+      	</div>        
+      	<div className="text">
+          <img role="presentation" src="postcard2.png" />
+          <img role="presentation" src="postcard4.png" />
+      	</div>
+      </div>      
+    );
+  }
+}
+
+export default What;

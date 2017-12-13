613b35ee6a6fa23b8cf27d78467fb6fffe868823
Kallirroi <kallirroi.retzepi@gmail.com>
2017-12-13 16:10:51 -0500

    added GIF for postcards

--- src/Cards.js
+++ src/Cards.js
@@ -8,9 +8,10 @@ class What extends Component {
         postcards
       	</div>        
       	<div className="text">
-          We really want to emphasize a sharing culture for the people who interact with the bench. We designed and printed a set of postcards like the ones below, drawn from the stories that are being narrated by the bench. The cards are free for our audience to mail or to drop inside a dedicated box in the library. We plan to periodically collect the cards and mail them across the network of all "bench-hosting" libraries.
+          We really want to emphasize a sharing culture for the people who interact with the bench. We designed and printed a set of postcards like the one below, drawn from the stories that are being narrated by the bench. The cards are free for anyone to mail or collect.
+
           <div className="images">
-            <img alt="" src="cards/image00001.png" />
+            <img alt="" src="cards/cards.gif" />
           </div>
       	</div>
       </div>      
